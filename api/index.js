8c2710dff8421df7f41cbf79292af67da332c5df
Parathps7 <77536966+Parathps7@users.noreply.github.com>
2024-02-26 12:34:47 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -62,6 +62,14 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  next();
+});
+
 app.use(cors());
 
 // TO send as JSON
